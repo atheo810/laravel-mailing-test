206efbe7bf5f030d53d39c1ea3166c9599390c86
atheo810 <atheos810@gmail.com>
2023-11-24 13:59:35 +0800

    feat: catch error validate

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="card mt-5">
+    <div class="main-content mt-5">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+        @endif
         <div class="card">
             <div class="card-header">
                 <div class="row">
@@ -14,25 +19,42 @@
                 </div>
 
             </div>
-        </div>
-        <div class="card-body">
-            <form action="">
-                <div class="form-group">
-                    <label for="" class="form-label">Image</label>
-                    <input type="file" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="" class="form-label">Title</label>
-                    <input type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="" class="form-label">Description</label>
-                    <textarea class="form-control" cols="20", rows="5"></textarea>
-                </div>
-                <div class="form-group mt-3">
-                    <button class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+
+            <div class="card-body">
+                <form action="{{ route('post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <div>
+                            <img src="{{ asset($post->image) }}" width="200" alt="">
+                        </div>
+                        <label for="" class="form-label">Image</label>
+                        <input type="file" class="form-control" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Title</label>
+                        <input type="text" class="form-control" value="{{ $post->title }}" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Category</label>
+                        <select id="" class="form-control" name="category_id">
+                            <option value="">Select...</option>
+                            @foreach ($categories as $category)
+                                <option {{ $category->id == $post->category_id ? 'selected' : '' }}
+                                    value="{{ $category->id }}">
+                                    {{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Description</label>
+                        <textarea class="form-control" cols="20", rows="5" name="description">{{ $post->description }}</textarea>
+                    </div>
+                    <div class="form-group mt-3">
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection

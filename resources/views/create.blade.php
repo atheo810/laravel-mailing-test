da42a17de59ce8a2770402d39f95466cd1433826
atheo810 <atheos810@gmail.com>
2023-11-24 09:58:26 +0800

    feat: parsing error create blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="card mt-5">
+    <div class="main-content     mt-5">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+        @endif
         <div class="card">
             <div class="card-header">
                 <div class="row">
@@ -12,27 +17,38 @@
                         <a href="{{ route('post.index') }}" class="btn btn-success mx-1">Back</a>
                     </div>
                 </div>
-
             </div>
-        </div>
-        <div class="card-body">
-            <form action="">
-                <div class="form-group">
-                    <label for="" class="form-label">Image</label>
-                    <input type="file" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="" class="form-label">Title</label>
-                    <input type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="" class="form-label">Description</label>
-                    <textarea class="form-control" cols="20", rows="5"></textarea>
-                </div>
-                <div class="form-group mt-3">
-                    <button class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+
+
+            <div class="card-body">
+                <form action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="" class="form-label">Image</label>
+                        <input type="file" class="form-control" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Title</label>
+                        <input type="text" class="form-control" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Category</label>
+                        <select id="" class="form-control" name="category_id">
+                            <option value="">Select...</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Description</label>
+                        <textarea name="description" class="form-control" cols="20", rows="5"></textarea>
+                    </div>
+                    <div class="form-group mt-3">
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection

1aec00e32b634e5403202453ed259599d28946d4
atheo810 <atheos810@gmail.com>
2023-11-25 22:42:32 +0800

    feat: route Group middleware

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'authCheck'], function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+
+    Route::get('/profile', function () {
+        return view('profile');
+    });
+});
+
+
+
+// Post Resource
 Route::get('/post/trash', [PostController::class, 'trashed'])->name('post.trashed');
 Route::get('/post/{id}/restore', [PostController::class, 'restore'])->name('post.restore');
 Route::delete('/post/{id}/force-delete', [PostController::class, 'forceDelete'])->name('post.force_delete');
-
 Route::resource('post', PostController::class);
 
+
+// testing middleware
 Route::get('/unavailable', function () {
     return view('unavailable');
 })->name('unavailable');

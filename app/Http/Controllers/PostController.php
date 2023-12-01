78318606619247912afb538119d53d0e2b819782
atheo810 <atheos810@gmail.com>
2023-12-01 12:43:39 +0800

    feat: caching database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
@@ -14,7 +15,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(5);
+        $posts = Cache::remember('posts', 60, function () {
+            return Post::with('category')->paginate(5);
+        });
         return view('index', compact('posts'));
     }
 
